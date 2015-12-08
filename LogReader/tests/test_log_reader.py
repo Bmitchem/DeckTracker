708a4104f3aa7225424afbd30d40b05f62d2eb0b
Robert Mitchem <=>
2015-12-08 18:59:28 -0500

    fixed tests running, they still fail. but that's supposed to happen

--- LogReader/tests/test_log_reader.py
+++ LogReader/tests/test_log_reader.py
@@ -18,8 +18,12 @@ class ReadFromFileTestCase(LogReaderTestCase):
 
 class TestTokenize(LogReaderTestCase):
     def setUp(self):
+        super(TestTokenize, self).setUp()
         test_string = "2015-12-03 21:53:33.355: [Zone] ZoneChangeList.ProcessChanges() - id=104 local=False [name=Mechanical Yeti id=35 zone=PLAY zonePos=3 cardId=GVG_078 player=2] pos from 4 -> 3"
         self.tokens = self.logReader._tokenize(test_string)
+
+    def test_tokens_created(self):
+        self.assertTrue(self.tokens)
 
     def test_number_of_tokens(self):
         self.assertEqual(len(self.tokens), 8)
