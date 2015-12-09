e0c22c7311be95475b4b6df567d31a9da7121e01
scavedo159 <scavedo159@gmail.com>
2015-12-08 19:50:10 -0600

    getting card info

--- LogReader/logreader.py
+++ LogReader/logreader.py
@@ -1,4 +1,5 @@
 import io
+import re
 
 
 class LogReader:
@@ -11,5 +12,24 @@ class LogReader:
                 print line
 
     def _tokenize(self, string):
-        parts = string.split()
+        parts = {}
+        parts['card'] = self.get_card(string)
+        print parts['card']
+        return parts
 
+    def get_card(self, string):
+        card_properties = ['name', 'id', 'zone', 'zonePos', 'cardId', 'player']
+        card = {}
+
+        start = string.rindex('[') + 1
+        end = string.rindex(']')
+        card_string = string[start:end]
+
+        for card_property in card_properties:
+            card[card_property] = self.get_card_property(card_string, card_property)
+
+        return card
+
+    def get_card_property(self, card_string, card_property):
+        m = re.search('(?<=%s=)\w+' % card_property, card_string)
+        return m.group(0)
--- LogReader/tests/test_log_reader.py
+++ LogReader/tests/test_log_reader.py
@@ -9,18 +9,22 @@ from LogReader.logreader import LogReader
 
 LOG_LOCATION = os.path.join(os.pardir, 'match.log')
 
+
 class LogReaderTestCase(unittest.TestCase):
     def setUp(self):
         self.logReader = LogReader(log_file=LOG_LOCATION)
 
+
 class ReadFromFileTestCase(LogReaderTestCase):
     pass
+
 
 class TestTokenize(LogReaderTestCase):
     def setUp(self):
         super(TestTokenize, self).setUp()
         test_string = "2015-12-03 21:53:33.355: [Zone] ZoneChangeList.ProcessChanges() - id=104 local=False [name=Mechanical Yeti id=35 zone=PLAY zonePos=3 cardId=GVG_078 player=2] pos from 4 -> 3"
         self.tokens = self.logReader._tokenize(test_string)
+        print self.tokens
 
     def test_tokens_created(self):
         self.assertTrue(self.tokens)
